c60964442c7df7b0ba5e28741f2d5661bb824fac
Alberto Soragna <alberto.soragna@gmail.com>
2024-06-27 10:13:31 -0700

    Imported upstream version '0.0.5' of 'upstream'

--- create3_teleop/setup.py
+++ create3_teleop/setup.py
@@ -6,7 +6,7 @@ package_name = 'create3_teleop'
 
 setup(
     name=package_name,
-    version='0.0.4',
+    version='0.0.5',
     packages=[],
     data_files=[
         (os.path.join('share','ament_index', 'resource_index', 'packages'),
